4ad0d3a7c65efb5ed7f4c9494703d5036bb73e2c
randomscot22 <97089145+randomscot22@users.noreply.github.com>
2026-05-27 12:01:25 +0100

    Enhance workingcommands.sh with test and MP4 commands
    Added default test code and MP4 file creation command.

--- workingcommands.sh
+++ workingcommands.sh
@@ -14,5 +14,10 @@ media-ctl -d $device -V ''\''csi2'\'':0 [fmt:BGR888_1X24/1920x1080 field:none co
 media-ctl -d $device -V ''\''csi2'\'':4 [fmt:BGR888_1X24/1920x1080 field:none colorspace:srgb]'
 v4l2-ctl -v width=1920,height=1080,pixelformat=BGR3
 
+# This is the default test code
 v4l2-ctl --verbose -d /dev/video0 --set-fmt-video=width=1920,height=1080,pixelformat='BGR3' --stream-mmap=4 --stream-skip=3 --stream-count=2 --stream-to=csitest.yuv --stream-poll
+
+# This code creates an MP4 file that can be video by VLC..
+ffmpeg -f v4l2 -video_size 1920x1080 -pixel_format bgr24 -i /dev/video0 -t 5 test.mp4
+
 
